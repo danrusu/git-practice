f397a20d34366afb3527e6e7d9b8732015318da5
Dan Rusu <dangingko@yahoo.com>
2022-09-01 18:12:53 +0300

    add demo test for husky

---
+++ test/demo.spec.js
@@ -0,0 +1,6 @@
+describe('Demo suite', () => {
+  it('Demo test', () => {
+    //throw new Error('intended failure'); // uncomment to fail the husky pre-commit hook
+    console.log('test passed');
+  });
+});
