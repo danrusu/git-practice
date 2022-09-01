97a76fbd478d5b72477d470acb31d0903a6705dd
Dan Rusu <dangingko@yahoo.com>
2022-09-01 18:23:52 +0300

    add TS test

--- test/demo.spec.js
+++ test/demo.spec.js
@@ -1,5 +1,5 @@
-describe('Demo suite', () => {
-  it('Demo test', () => {
+describe('Demo JS suite', () => {
+  it('Demo JS test', () => {
     //throw new Error('intended failure'); // uncomment to fail the husky pre-commit hook
     console.log('test passed');
   });
